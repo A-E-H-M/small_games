579f9014c84190d34d330c87306bd4be40b20a1e
alexandria <alexandria.hmann@gmail.com>
2024-10-05 17:32:58 -0500

    Add font & text

--- timber.cpp
+++ timber.cpp
@@ -1,4 +1,6 @@
 // Include important libraries here
+#include <sstream>
+
 #include <SFML/Graphics.hpp>
 
 // Make code easier to type with "using namespace" 
@@ -93,7 +95,44 @@ int main()
 	float cloud3Speed {0.0f};
 
 	// Variables to control time itself
-	Clock clock;	
+	Clock clock;
+
+	// Track whether the game is running
+	bool paused {true};
+
+	// Draw some text
+	int score {0};
+
+	Text messageText;
+	Text scoreText;
+
+	// We need to choose a font
+	Font font;
+	font.loadFromFile("fonts/KOMIKAP.ttf");
+
+	// Set the font to our message
+	messageText.setFont(font);
+	scoreText.setFont(font);
+
+	// Assign the actual message
+	messageText.setString("Press Enter to start!");
+	scoreText.setString("Score = 0");
+
+	// Make it really big
+	messageText.setCharacterSize(75);
+	scoreText.setCharacterSize(100);
+
+	// Choose a color
+	messageText.setFillColor(Color::White);
+	scoreText.setFillColor(Color::White);
+
+	// Position the text
+	FloatRect textRect = messageText.getLocalBounds();
+	messageText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+
+	messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+
+	scoreText.setPosition(20, 20);
 	
 	while (window.isOpen())
 	{
@@ -109,11 +148,20 @@ int main()
 			window.close();
 		}
 
+		if (Keyboard::isKeyPressed(Keyboard::Return))
+		{
+			paused = false;
+		}
+
 		/*
 		**************************************** 
 		Update the scene
 		****************************************
 		*/
+
+		// Start the game
+		if (!paused)
+		{
 
 		// Measure time
 		Time dt = clock.restart();
@@ -226,6 +274,13 @@ int main()
 			}
 
 		}
+		
+		// Update the score text
+		std::stringstream ss;
+		ss << "Score = " << score;
+		scoreText.setString(ss.str());
+
+		} // End if(!paused)
 
 		/*
 		**************************************** 
@@ -249,6 +304,14 @@ int main()
 
 		// Draw the bee
 		window.draw(spriteBee);
+
+		// Draw the score
+		window.draw(scoreText);
+		if (paused)
+		{
+			// Draw our message
+			window.draw(messageText);
+		}
 
 		// Show everything we just drew 
 		window.display();
