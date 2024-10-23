0ab73b7a7acfb3cb3e2da3667f3750ea839215c4
alexandria <alexandria.hmann@gmail.com>
2024-10-23 13:22:18 -0500

    Added ball class

--- pretty_pretty_pong/pretty_pretty_pong_main.cpp
+++ pretty_pretty_pong/pretty_pretty_pong_main.cpp
@@ -3,6 +3,7 @@
 #include <SFML/Graphics.hpp>
 
 #include "bat.hpp"
+#include "ball.hpp"
 
 int main()
 {
@@ -18,6 +19,9 @@ int main()
 
 	// Create a bat at the bottom center of the screen
 	bat m_Bat(1920 / 2, 1080 - 20);
+
+	// Create a ball
+	Ball ball(1920 / 2, 0);
 	
 	// Create a text object called HUD
 	Text hud;
@@ -80,15 +84,60 @@ int main()
 		// Update the delta time
 		Time dt = clock.restart();
 		m_Bat.update(dt);
+		ball.update(dt);
+
 		// Update the HUD text
 		std::stringstream ss;
 		ss << "Score: " << score << " Lives: " << lives;
 		hud.setString(ss.str());
 
+		// Handle ball hitting the bottom on the window
+		if (ball.getPosition().top > window.getSize().y)
+		{
+			// Reverse the ball direction
+			ball.reboundBottom();
+
+			//Remove a life
+			lives--;
+
+			// Check for zero lives
+			if (lives <  1)
+			{
+				// Reset the score
+				score = 0;
+
+				// Reset the lives
+				lives = 3;
+			}
+		}
+		
+		// Handle ball hitting the top of the window
+		if (ball.getPosition().top < 0)
+		{
+			ball.reboundBatOrTop();
+
+			// Add a point to the players score
+			score++;
+		}
+
+		// Handle ball hitting sides of the window
+		if (ball.getPosition().left < 0 || ball.getPosition().left + ball.getPosition().width > window.getSize().x)
+		{
+			ball.reboundSides();
+		}
+
+		// Has the ball hit the bat?
+		if (ball.getPosition().intersects(m_Bat.getPosition()))
+		{
+			// Hit detected so reverse the ball and score a point
+			ball.reboundBatOrTop();
+		}
+
 		// Draw the bat, the ball and HUD
 		window.clear();
 		window.draw(hud);
 		window.draw(m_Bat.getShape());
+		window.draw(ball.getShape());
 		window.display();
 
 	} // End main game loop
